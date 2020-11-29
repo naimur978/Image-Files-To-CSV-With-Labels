565c30e7a143685d64a9a99dd0dc0aac3ae43fdf
Naimur Rahman <naimur978@gmail.com>
2020-11-30 00:11:56 +0600

    adds header

--- convert.py
+++ convert.py
@@ -11,8 +11,11 @@ with open('output.csv', 'w', newline='') as csvfile:
             if '.jpg' in _file:
                 files.append(_file)
 
+    header = ["image_id", "label"]
     # Create a writer from csv module
     writer = csv.writer(csvfile, delimiter=',')
+    writer.writerow(i for i in header)
+
     for f in files:  # find type of file
         # cut off the number and .jpg from file, leaving only the type (this may have to be changed.)
         t = f[0:-4]
